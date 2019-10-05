278a7c4d79f77f4d0375f2c13c78991d47f8321b
Anne Ogborn <annie66us@yahoo.com>
2019-10-05 18:43:05 +0530

    quick push for pbm

--- prolog/game.pl
+++ prolog/game.pl
@@ -21,39 +21,38 @@ server(Port) :-
 
 :- chr_constraint
     chr_reset/1,
-    touch/3,
-    costume/3,
-    get_costume/3,
-    init_player/1.
+    start_draw/1,
+    do_end_stroke/1,
+    do_end_draw/1,
+    do_add_coord/2,
+    x/2,
+    point/3,
+    start_point/3,
+    last_point/3,
+    line/5,
+    get_a_line/5,
+    inited/1,
+    unmatched_end_stroke/1.
 
-:- http_handler('/touch', touched , []).
+:- http_handler('/drawing', draw_handler , []).
 
-touched(Request) :-
+draw_handler(Request) :-
     http_parameters(Request,
                 [
-                    me(Me, []),
-                    dest(Dest, []),
-                    sess(S, [integer])
+                    sess(S, [integer]),
+                    drawing(Drawing, [string])
                 ]),
     init_session(S),
-    do_in_chr_thread(touch(S, Me, Dest), get_costume(S, Me, Costume)),
+    split_string(Drawing, "|", " ", Strokes),
+    do_in_chr_thread(start_draw(S), get_dummy(_)),
+    maplist(do_stroke_elem(S), Strokes),
+    do_in_chr_thread(do_end_draw(S), get_dummy(_)),
     format('Access-Control-Allow-Origin: *~n'),
-    format('Content-type: text/plain~n~n~w', [Costume]).
+    format('Content-type: text/plain~n~nok').
 
-
-:- http_handler('/costume', respond_current_costume , []).
-
-respond_current_costume(Request) :-
-    http_parameters(Request,
-                [
-                    me(Me, []),
-                    sess(S, [integer])
-                ]),
-    init_session(S),
-    do_in_chr_thread(true, get_costume(S, Me, Costume)),
-    format('Access-Control-Allow-Origin: *~n'),
-    format('Content-type: text/plain~n~n~w', [Costume]).
-
+do_stroke_elem(S, NString) :-
+    number_string(N, NString),
+    do_in_chr_thread(do_add_coord(S, N), get_dummy(_)).
 
 :- http_handler('/reset', do_reset , []).
 
@@ -68,8 +67,7 @@ do_reset(Request) :-
     format('Content-type: text/plain~n~nOK').
 
 reset_session(S) :-
-    do_in_chr_thread(chr_reset(S), get_dummy(_)),
-    do_in_chr_thread(costume(S, egg, shell), get_dummy(_)).
+    do_in_chr_thread(chr_reset(S), get_dummy(_)).
 
 init_session(S) :-
     do_in_chr_thread(init_player(S), get_dummy(_)).
@@ -78,30 +76,65 @@ init_session(S) :-
 		 *          Game Logic          *
 		 *******************************/
 
+% prolog to provide a dummy return from chr thread
 get_dummy(ok).
 
-chr_reset(S) \ costume(S, _, _) <=> true.
-chr_reset(S) \ touch(S, _, _) <=> true.
-chr_reset(S) \ get_costume(S, _, _) <=> true.
-chr_reset(_) <=> true.
+% reset to the start of game state. Not same as init_player
+% which establishes initial conditions when session first seen
+chr_reset(_) <=> true. % temp, not storing any state long term now
+
+% start a 'drawing' - single http set of lines
+start_draw(S) \ x(S, _) <=> true.
+start_draw(S) \ point(S, _, _) <=> true.
+start_draw(S) \ start_point(S, _, _) <=> true.
+start_draw(S) \ last_point(S, _, _) <=> true.
+start_draw(S) \ start_draw(S) <=> true. % poorly formated last req
+start_draw(_) <=> true.
+% hang on to start_draw til we see a point
+
+% add one more coordinate point to the store
+do_add_coord(S, Y), x(S, X) <=> point(S, X, Y).
+do_add_coord(S, X) <=> x(S, X).
+
+point(S, 10000, 10000) <=> do_end_stroke(S).
+point(S, X, Y), last_point(S, _, _) <=> last_point(S, X, Y).
+point(S, X, Y) <=> start_point(S, X, Y), last_point(S, X, Y).
+
+do_end_stroke(S), start_point(S, SX, SY), last_point(S, EX, EY) <=>
+    line(S, SX, SY, EX, EY).
+do_end_stroke(_) <=> true.
+
+do_end_draw(S) ==> debug_lines(S).
+do_end_draw(S) \ line(S, _, _, _, _) <=> true.
+do_end_draw(S) \ start_draw(S) <=> true.
+do_end_draw(_) <=> true.
+
+% get_foo pattern to get the lines
+line(S, SX, SY, EX, EY), get_a_line(S, GSX, GSY, GEX, GEY) ==>
+    SX = GSX,
+    SY = GSY,
+    EX = GEX,
+    EY = GEY.
+get_a_line(_, _, _, _, _) <=> true.
 
 % set up player if we haven't seen them
 % idempotic pattern
 % if we've already got a costume we inited already
 % only add costume if we didn't have one
-costume(S, _, _) \ init_player(S) <=> true.
-init_player(S) <=> costume(S, egg, shell).
+init_player(S) :-
+    inited(S).
 
-% when I touch the pan I go from shell to fried egg
-touch(S, egg, pan) \ costume(S, egg, shell) <=> costume(S, egg, friedegg).
-
-% add get_costume with Y unbound to retrieve X's costume
-costume(S, X, Current) \ get_costume(S, X, Is) <=> Is = Current.
-get_costume(_, _, _) <=> fail.
+inited(S) \ inited(S) <=> true.
 
 		 /*******************************
 		 * Debug help                   *
 		 *******************************/
+debug_lines(S) :-
+    debug(lines, '====', []),
+    find_chr_constraint(line(S, SX, SY, EX, EY)),
+    debug(lines, '~w,~w -- ~w,~w', [SX, SY, EX, EY]),
+    fail.
+debug_lines(_).
 
 
 debug_constraints(Where) :-
@@ -123,6 +156,7 @@ create_chr_thread :-
 polling_sub :-
    % listen for new message on `sub` queue
    thread_get_message(sub, sync(ActionCHR, ResultCHR)),
+   debug_constraints(polling_sub),
    % do the actual constraint call
    (   call(ActionCHR)
    ;
@@ -170,3 +204,4 @@ do_in_chr_thread(ActionCHR, ResultCHR) :-
    thread_get_message(par, Result).
 
 :- debug(constraint(_)).
+:- debug(lines).
